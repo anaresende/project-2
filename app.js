913ea107a2f3a38b0adb22b675d718ef0dc727c8
Juandi Mena <juandimena34@gmail.com>
2021-09-09 19:21:02 +0200

    template and api folder added

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
+require('dotenv').config()
+
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-const PORT = 3000;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// Functional curling style of loading configuration
+require('./config/db')
+require('./config/global')(app)
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -38,7 +31,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-app.listen(PORT)
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+const User = require("../models/User.model")
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Project Name' });
-});
-
-router.get('/catarina', function(req, res, next) {
-  res.render('index', { title: 'Project Name' });
+  User.find().then((users)=>
+  res.render('index', { title: 'Express', users})
+  )
 });
 
 module.exports = router;
