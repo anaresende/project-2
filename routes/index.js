8920ec07ad7bc3b1cb687800b3855a4fdbe6cb48
JuandiM <juandimena34@gmail.com>
2021-09-12 18:23:18 +0200

    feat:add api

--- routes/index.js
+++ routes/index.js
@@ -9,37 +9,4 @@ router.get('/', function(req, res, next) {
 
 
 
-
-
-// var axios = require("axios").default;
-
-// var options = {
-//   method: 'GET',
-//   url: 'https://data-imdb1.p.rapidapi.com/genres/',
-//   headers: {
-//     'x-rapidapi-host': 'data-imdb1.p.rapidapi.com',
-//     'x-rapidapi-key': '120cbfcc78msh7bcbf5a09929b0cp15795cjsnf489b5a4ae37'
-//   }
-// };
-
-// axios.request(options).then(function (response) {
-// 	console.log(response.data);
-// }).catch(function (error) {
-// 	console.error(error);
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 module.exports = router;
--- routes/movies.route.js
+++ routes/movies.route.js
@@ -2,34 +2,44 @@ const express = require('express');
 const router = express.Router();
 
 const Movie = require('../models/moviemodel');
+const PopcornApi = require('../api/api')
 
 //For actors and directors
 // const Actors = require('../models/actorsmodel');
 // const Directors = require('../models/directorsmodel');
 
-//Movie list
-router.get('/', (req, res) => {
-	Movie.find()
-		//.populate('') => in case we have the actors and directors, we can populate them
-		.then((allMovies) => {
-			console.log(allMovies);
-			res.render('movies/movie-list', { allMovies });
-		})
-		.catch((err) => console.log(err));
-});
+router.get('/', (req, res)=> {
+	console.log('enter search page')
+	PopcornApi.getMovieBySearch('la la land')
+	.then((search) =>{
+		console.log(search)
+		res.render('movies/movie-list', {movies: search.results})
+	})
+ });
 
-//Each movie
-router.get('/:id', (req, res) => {
-	const { id } = req.params;
+// //Each movie
+// router.get('/:id', (req, res) => {
+// 	const { id } = req.params;
 
-	Movie.findById(id)
-		//.populate('') => in case we have the actors and directors, we can populate them
-		.then((movie) => {
-			console.log(movie);
-			res.render('movies/each-movie', { movie });
-		})
-		.catch((err) => console.log(err));
-});
+// 	Movie.findById(id)
+// 		//.populate('') => in case we have the actors and directors, we can populate them
+// 		.then((movie) => {
+// 			console.log(movie);
+// 			res.render('movies/each-movie', { movie });
+// 		})
+// 		.catch((err) => console.log(err));
+// });
+
+
+//API SEARCH
+//  router.get('/movie-detail', (req, res)=> {
+// 	 PopcornApi.getOneMovie('680')
+
+// 	 .then((id)=> {
+// 		 console.log (id)
+// 		 res.render('movies/each-movie', {movie: id})
+// 	})
+//  });
 
 
 
