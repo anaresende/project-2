388dd41a8d99e993dbb70e11e2e2cf676b36b027
Catarina Resende <41744553+anaresende@users.noreply.github.com>
2021-09-15 08:30:48 +0100

    feat: styled user profile and add content to homepage

--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,36 @@ const PopcornApi = require('../api/api');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const user = req.session.currentUser
+  
   PopcornApi.getUpcomingMovies()
     .then((upcomingMovies) => {
-      res.render('index', { 
-        title: 'Project Name',
-        user,
-        upcomingMovies: upcomingMovies.results 
-      });
+
+      PopcornApi.getTopRatedMovies()
+        .then((topRatedMovies) => {
+          res.render('index', { 
+            title: 'Project Name',
+            user,
+            topRatedMovies: topRatedMovies.results,
+            upcomingMovies: upcomingMovies.results 
+          });
+        })
+      // res.render('index', { 
+      //   title: 'Project Name',
+      //   user,
+      //   upcomingMovies: upcomingMovies.results 
+      // });
     })
+
+  
       
 });
 
-module.exports = router;
 
 
 router.get('/about-us', (req, res)=> {
   res.render('about-us/about-us')
 })
+
+
+
+module.exports = router;
